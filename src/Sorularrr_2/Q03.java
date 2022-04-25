45b2294af75b8b680be48e57a8fbb5978f2e4599
Serap Diraz <emeksevde74@gmail.com>
2022-04-25 14:02:43 +0300

    25.04.2022

---
+++ src/Sorularrr_2/Q03.java
@@ -0,0 +1,24 @@
+package Sorularrr_2;
+
+import java.util.Scanner;
+
+public class Q03 {
+    // kullanicidan 2 tam sayi alip bu 2 tamsayinin  dort islem sonucunu yazdiriniz.
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Birinci sayiyi giriniz");
+        double sayi1=scan.nextDouble();
+
+        System.out.println("İkinci sayiyi giriniz");
+        double sayi2=scan.nextDouble();
+
+        System.out.println("Toplam = " + (sayi1+sayi2));
+        System.out.println("Fark = " + (sayi1-sayi2));
+        System.out.println("Carpim = " + (sayi1*sayi2));
+        System.out.println("Bolum = " + (sayi1/sayi2));
+
+scan.close();
+
+    }
+}
---
+++ src/Sorularrr_2/Q06.java
@@ -0,0 +1,33 @@
+package Sorularrr_2;
+
+import java.util.Scanner;
+
+public class Q06 {
+
+    /*  Problem Tanımı
+        Mesafeyi ve hızı alıp süreyi hesaplayan bir kod yazınız.
+        Örneğin:İstanbul ile Ankara arası 400km olarak ölçülmektedir. Bu yolu ortalama
+        120 km/saat hızla giden bir araç ne kadar sürede hedefe varır?
+
+        Örnek Ekran Çıktısı
+        Mesafeyi giriniz: 400
+        Hızı giriniz: 100
+        Süre 4 saattir.
+     */
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Mesafeyi giriniz");
+        double mesafe=scan.nextDouble();
+
+        System.out.println("Ortalama hizinizi giriniz");
+        double ortHiz=scan.nextDouble();
+
+        double varisSure=mesafe/ortHiz;
+
+        System.out.println("Varis sureniz : " + varisSure);
+
+    }
+
+}
